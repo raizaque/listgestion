1237b2ebe28c7ca58e6121e80b1e7205961cb1cc
raizaque <barzizouiyahya@gmail.com>
2018-03-15 16:59:37 +0100

    test class

--- app/src/main/java/com/example/barzi/application/Utilisateur/Affiche_list.java
+++ app/src/main/java/com/example/barzi/application/Utilisateur/Affiche_list.java
@@ -4,16 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.barzi.application.R;
 
 public class Affiche_list extends AppCompatActivity {
 
+    private TextView titre;
+    private TextView description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affiche_list);
         Spinner spinner=(Spinner)findViewById(R.id.spinner);
+        titre=(TextView)findViewById(R.id.Titreliste);
+        description=(TextView)findViewById(R.id.description);
+
 
         String[] visibility=getResources().getStringArray(R.array.array_visibilité);
 
